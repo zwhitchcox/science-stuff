e2abf5e4e45fa16dd196f42fdeed76b6073cb67f
Zane Hitchcox <zwhitchcox@gmail.com>
2020-05-15 10:46:22 -0400

    science stuff

---
+++ combinatorics/n-to-the-k.py
@@ -0,0 +1,8 @@
+import sys
+
+# program to count password possibilities
+
+numChars = int(input("Enter number of characters allowed in password (e.g. A-Z, a-z, 0-9 = 62): "))
+passLen = int(input("Enter length of password: "))
+
+print(f'The number of possible characters is {numChars**passLen}.')
