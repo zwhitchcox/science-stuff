7ad5505b98ef2e3e4ecb2e412a72ee249717304f
Zane Hitchcox <zwhitchcox@gmail.com>
2020-05-15 13:01:16 -0400

    combinatorics - 0030_password-no-repeat

--- combinatorics/0030_password-no-repeat.py
+++ combinatorics/0030_password-no-repeat.py
@@ -1,8 +1,7 @@
 import sys
-
-# program to count password possibilities
+import math
 
 numChars = int(input("Enter number of characters allowed in password (e.g. A-Z, a-z, 0-9 = 62): "))
 passLen = int(input("Enter length of password: "))
 
-print(f'The number of possible characters is {numChars**passLen:,}.')
+print(f'The number of possible characters with no repeating characters is {int(math.factorial(numChars)/math.factorial(numChars - passLen)):,}.')
